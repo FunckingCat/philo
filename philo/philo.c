bba356362b83630dc52dc996d8be1b5e934d0de8
David Zhuravlev <david99111@mial.ru>
2022-01-05 13:04:36 +0300

    final_fix

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: unix <unix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 16:15:49 by unix              #+#    #+#             */
-/*   Updated: 2022/01/02 18:30:45 by unix             ###   ########.fr       */
+/*   Updated: 2022/01/05 13:02:53 by unix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,12 @@ void	*philosoph(void *ptr)
 	self->death_lim = self->last_eat + self->state->tm_die;
 	pthread_create(&tid, NULL, observer, ptr);
 	pthread_detach(tid);
-	if (self->name % 2 == 1)
+	if (self->name % 2 == 1 || (self->state->amount % 2 == 1
+			&& self->name == self->state->amount - 1))
 	{
-		massage(self, SLEEPING);
-		usleep(self->state->tm_sleep * 1000);
-	}
-	else
 		massage(self, THINKING);
+		usleep(self->state->tm_eat * 500);
+	}
 	while (1)
 	{
 		eat(self);
